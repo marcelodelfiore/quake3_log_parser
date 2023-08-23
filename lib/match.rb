3edc38e5d11b419daac1442cbd84117456b667bd
Marcelo Del Fiore <marcelodelfiore@gmail.com>
2023-08-23 12:03:48 -0300

    Update lib classes

--- lib/match.rb
+++ lib/match.rb
@@ -2,4 +2,28 @@
 
 # Match class for parsing Quake3 log files
 class Match
+  attr_accessor :number_of_kills, :players, :kills
+
+  def initialize
+    @number_of_kills = 0
+    @players = []
+    @kills = {}
+  end
+
+  def add_kill(entry_data)
+    if entry_data[:killer] == '<world>'
+      killed_loses_one_kill(entry_data[:killed])
+    else
+      @players << entry_data[:killer] unless @players.include?(entry_data[:killer])
+      @players << entry_data[:killed] unless @players.include?(entry_data[:killed])
+      @kills.key?(entry_data[:killer]) ? @kills[entry_data[:killer]] += 1 : @kills[entry_data[:killer]] = 1
+    end
+    @number_of_kills += 1
+  end
+
+  def killed_loses_one_kill(player)
+    return unless @kills.key?(player)
+
+    @kills[player] -= 1 if (@kills[player]).positive?
+  end
 end
--- lib/quake3_log_parser.rb
+++ lib/quake3_log_parser.rb
@@ -5,5 +5,73 @@ require_relative 'match'
 
 # Main class for parsing Quake3 log files
 class Quake3LogParser
+  attr_accessor :matches, :current_match
 
+  def initialize
+    @matches = []
+    @current_match = nil
+  end
+
+  def parse(filename)
+    file = File.new(filename)
+
+    while (file_line = file.gets)
+      entry_data = decode_log_entry(file_line)
+      current_match.add_kill(entry_data) if entry_data
+    end
+
+    parse_presenter
+  rescue Errno::ENOENT => err
+    "Could not find file: #{filename}"
+    raise err
+  end
+
+  private
+
+  def decode_log_entry(entry)
+    if entry =~ ParserRules.entry_a_kill?
+      parse_kill_entry(entry)
+    elsif entry =~ ParserRules.init_game?
+      set_new_match
+      nil
+    elsif entry =~ ParserRules.shutdown_game?
+      finish_parsing
+      nil
+    end
+  end
+
+  def parse_kill_entry(entry)
+    kill_entry = entry.split(':').last
+    kill_data = kill_entry.split(ParserRules.kill_entry_split)
+
+    {
+      killer: kill_data[0].strip,
+      killed: kill_data[1].strip,
+      reason: kill_data[2].strip
+    }
+  end
+
+  def set_new_match
+    @matches << current_match
+    @current_match = Match.new
+  end
+
+  def finish_parsing
+    @matches << current_match
+    @matches.compact!
+    @current_match = nil
+  end
+
+  def parse_presenter
+    results = {}
+    matches.each_with_index do |match, index|
+      results["game_#{index + 1}"] =
+        {
+          total_kills: match.number_of_kills,
+          players: match.players,
+          kills: match.kills
+        }
+    end
+    results
+  end
 end
